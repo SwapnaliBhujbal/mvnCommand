ca2d1b15c855c361d1a94073ffb7743321d033df
Swapnali Bhujbal <swapnalibhujbal596@gmail.com>
2023-06-10 22:00:50 +0530

    first commit

---
+++ mavenComma/src/test/java/org/com/mavenPr/fTest.java
@@ -0,0 +1,17 @@
+package org.com.mavenPr;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class fTest {
+	@Test
+	public void login() {
+	
+		System.out.println("login");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+	}
+
+}
+
